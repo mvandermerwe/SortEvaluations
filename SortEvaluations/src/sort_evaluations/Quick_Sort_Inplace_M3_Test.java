7c732b4809777f138071eea3b8748446c2da2fad
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-02-16 16:48:02 -0700

    Some additional testing for QSM3

--- SortEvaluations/src/sort_evaluations/Quick_Sort_Inplace_M3_Test.java
+++ SortEvaluations/src/sort_evaluations/Quick_Sort_Inplace_M3_Test.java
@@ -35,6 +35,36 @@ public class Quick_Sort_Inplace_M3_Test {
 		quickSort.sort(intArray);
 		assertArrayEquals(intShouldBe.toArray(), intArray.toArray());
 	}
+	
+	/**
+	 * Simple test case of small array for naive quick sort.
+	 */
+	@Test
+	public void testSortAgain() {
+		Quick_Sort_Inplace_M3<Integer> quickSort = new Quick_Sort_Inplace_M3<>();
+		quickSort.set_constant(3);
+
+		ArrayList<Integer> intArray = new ArrayList<>();
+		intArray.add(6);
+		intArray.add(5);
+		intArray.add(4);
+		intArray.add(3);
+		intArray.add(2);
+		intArray.add(1);
+		
+
+		ArrayList<Integer> intShouldBe = new ArrayList<>();
+		intShouldBe.add(1);
+		intShouldBe.add(2);
+		intShouldBe.add(3);
+		intShouldBe.add(4);
+		intShouldBe.add(5);
+		intShouldBe.add(6);
+
+		quickSort.sort(intArray);
+		assertArrayEquals(intShouldBe.toArray(), intArray.toArray());
+	}
+
 
 	/**
 	 * Test running of sort w/ a zero array.
