94c2d5f1dbe05cf7fcdecf4cf7cd3c7a89be6b9e
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-02-16 16:18:34 -0700

    Added time constant change testing for required types.

--- SortEvaluations/src/sort_evaluations/Main.java
+++ SortEvaluations/src/sort_evaluations/Main.java
@@ -7,7 +7,6 @@ package sort_evaluations;
 import java.util.ArrayList;
 
 /**
- * FIXME: put your code to time all of your sorts here.
  * 
  * Pseudocode: built a list of sorters add all of the sorts (e.g.,
  * Quick_Sort_Naive, Quick_Sort_Inplace...) to the list for each element of the
@@ -36,10 +35,15 @@ public class Main {
 		Sort_Utils sort_utils = new Sort_Utils();
 
 		// For each sort, test its performance on increasingly sized data sets.
+		/*
 		for (Sorter<Integer> sort : sort_methods) {
-			sort_utils.test_and_time(sort, 0, 100000, 1000000000, 1200);
+			sort.set_constant(3);
+			sort_utils.test_and_time(sort, 0, 500000, 10000000, 600);
 		}
+		*/
 		
+		sort_utils.time_constant_change(sort_methods.get(2), 0, 1, 30, 200000);
+		sort_utils.time_constant_change(sort_methods.get(7), 0, 1, 30, 200000);
 		
 	}
 
--- SortEvaluations/src/sort_evaluations/Quick_Sort_Naive_Test.java
+++ SortEvaluations/src/sort_evaluations/Quick_Sort_Naive_Test.java
@@ -35,6 +35,35 @@ public class Quick_Sort_Naive_Test {
 		quickSort.sort(intArray);
 		assertArrayEquals(intShouldBe.toArray(), intArray.toArray());
 	}
+	
+	/**
+	 * Simple test case of small array for naive quick sort.
+	 */
+	@Test
+	public void testSortAgain() {
+		Quick_Sort_Naive<Integer> quickSort = new Quick_Sort_Naive<>();
+		quickSort.set_constant(3);
+
+		ArrayList<Integer> intArray = new ArrayList<>();
+		intArray.add(1);
+		intArray.add(2);
+		intArray.add(3);
+		intArray.add(4);
+		intArray.add(5);
+		intArray.add(6);
+		
+
+		ArrayList<Integer> intShouldBe = new ArrayList<>();
+		intShouldBe.add(1);
+		intShouldBe.add(2);
+		intShouldBe.add(3);
+		intShouldBe.add(4);
+		intShouldBe.add(5);
+		intShouldBe.add(6);
+
+		quickSort.sort(intArray);
+		assertArrayEquals(intShouldBe.toArray(), intArray.toArray());
+	}
 
 	/**
 	 * Test running of sort w/ a zero array.
